6a26e96b8c8a7c7017603084d458ad2a416e1c15
mariiaSychova <mariiasychova3@gmail.com>
2024-06-16 19:05:01 +0300

    fix login

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,19 +1,17 @@
-import { mockUser } from "@/mock-data";
-import { encrypt } from "@/utils/auth";
+import { decrypt } from "@/utils/auth";
 import { executeDBQuery } from "@/utils/db";
 
 export async function POST(req: Request) {
-  const { email, password: decryptedPassword } = (await req.json()) as {
+  const { email, password } = (await req.json()) as {
     email: string;
     password: string;
   };
 
-  const password = encrypt(decryptedPassword);
+  const users = await executeDBQuery(`SELECT * from public.employee`);
 
-  // const user = null
-  const user = mockUser;
-
-  // await executeDBQuery(`DELETE * from product WHERE id_product = ${id}`)
+  const user = users.find(
+    (u: any) => u.email === email && decrypt(u.password) === password
+  );
 
   return Response.json({ status: user ? "success" : "error", user });
 }
--- utils/auth.ts
+++ utils/auth.ts
@@ -3,9 +3,11 @@ import CryptoJS from "crypto-js";
 const SECRET_KEY = process.env.CRYPTO_SECRET_KEY || "some secret key";
 
 export const encrypt = (data: string) => {
-  return CryptoJS.AES.encrypt(data, SECRET_KEY).toString();
+  return CryptoJS.AES.encrypt(data, SECRET_KEY, {
+    mode: CryptoJS.mode.ECB,
+  }).toString();
 };
 
 export const decrypt = (data: string) => {
-  return CryptoJS.AES.decrypt(data, SECRET_KEY);
+  return CryptoJS.AES.decrypt(data, SECRET_KEY).toString(CryptoJS.enc.Utf8);
 };
