13df12bd84ed6c563af6a5ea9b549109ea6ed37c
Rudnyk Bohdan <rudnikboda@gmail.com>
2024-06-20 16:18:09 +0300

    added new api for product

--- API/products.ts
+++ API/products.ts
@@ -1,25 +1,33 @@
-import { TProduct, TProduct_Optional } from "@/types"
-import axios from "axios"
+import { TProduct, TProduct_Optional } from "@/types";
+import axios from "axios";
 
 export const getAllProductsInnerRoute = async (): Promise<TProduct[]> => {
-    const data = await axios.get("/api/products/get-all")
+  const data = await axios.get("/api/products/get-all");
 
-    return data.data
-}
+  return data.data;
+};
 
 export const createProductInnerRoute = async (
-    data: TProduct
+  data: TProduct
 ): Promise<void> => {
-    await axios.post("/api/products/create", data)
-}
+  await axios.post("/api/products/create", data);
+};
 
 export const updateProductInnerRoute = async (
-    id: number,
-    data: TProduct_Optional
+  id: number,
+  data: TProduct_Optional
 ): Promise<void> => {
-    await axios.post("/api/products/create", { id, data })
-}
+  await axios.post("/api/products/create", { id, data });
+};
 
 export const deleteProductInnerRoute = async (id: number): Promise<void> => {
-    await axios.post("/api/products/delete", { id })
-}
+  await axios.post("/api/products/delete", { id });
+};
+
+export const getAllProductsForDisplayInnerRoute = async (): Promise<
+  TProduct[]
+> => {
+  const data = await axios.get("/api/products/get-all-for-display");
+
+  return data.data;
+};
