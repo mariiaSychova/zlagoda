ede194a25e071a4a0fbff87c4ad4266f08e8f4d1
Rudnyk Boh <rudnikboda@gmail.com>
2024-06-20 03:26:45 +0300

    added api for store-product

--- API/store-product.ts
+++ API/store-product.ts
@@ -1,1 +1,28 @@
+import axios from "axios";
+import { TStoreProduct, TStoreProduct_Optional } from "@/types";
 
+export const getAllStoreProductsInnerRoute = async (): Promise<
+  TStoreProduct[]
+> => {
+  const response = await axios.get("/api/store-products/get-all");
+  return response.data;
+};
+
+export const createStoreProductInnerRoute = async (
+  data: TStoreProduct
+): Promise<void> => {
+  await axios.post("/api/store-products/create", data);
+};
+
+export const updateStoreProductInnerRoute = async (
+  id: number,
+  data: TStoreProduct_Optional
+): Promise<void> => {
+  await axios.post("/api/store-products/update", { id, data });
+};
+
+export const deleteStoreProductInnerRoute = async (
+  id: number
+): Promise<void> => {
+  await axios.post("/api/store-products/delete", { id });
+};
